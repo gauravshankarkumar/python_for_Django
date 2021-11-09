3b8c646271f2e19397ce92b77ea08db104a92633
Gaurav Shankar Kumar <gaurav@Gauravs-MacBook-Pro.local>
2021-11-09 16:55:09 +0530

    Variables, Strings, Ints and Print

---
+++ 1.py
@@ -0,0 +1,9 @@
+age = 27
+
+name = "Gaurav"
+
+print("Hi my name is {} and I am {} years old".format(name, age))
+
+print("Hi my name is {0} and I am {1} years old".format(name, age))
+
+print(f"Hi my name is {name} and I am {age} years old")
