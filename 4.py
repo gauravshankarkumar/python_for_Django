7d878645b1559f1edf4f160d1fc758786a2d0eb3
Gaurav Shankar Kumar <gaurav@Gauravs-MacBook-Pro.local>
2021-11-09 19:20:58 +0530

    If Statements and Comments

---
+++ 4.py
@@ -0,0 +1,23 @@
+age = 17
+# name = 'gaurav'
+# name = 'vasu'
+
+gaurav = False
+vasu = True
+
+if gaurav:
+    print('Hi Gaurav')
+
+else:
+    print('Hi Alien')
+
+#new statement
+
+gaurav = True
+vasu = False
+
+if gaurav:
+    print('Hi Gaurav')
+
+else:
+    print('Hi Alien')
